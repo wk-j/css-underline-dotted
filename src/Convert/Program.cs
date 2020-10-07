08f435bfec3951c0af8c825e615abc045f468d9b
wk <somnuk.wk@gmail.com>
2020-10-07 12:59:05 +0700

    Convert

---
+++ src/Convert/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using DinkToPdf;
+
+namespace Convert {
+    class Program {
+        static void Main(string[] args) {
+            var doc = new HtmlToPdfDocument() {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Landscape,
+                    PaperSize = PaperKind.A4Plus,
+                },
+                Objects = {
+                    new ObjectSettings() {
+                        PagesCount = true,
+                        HtmlContent = File.ReadAllText("index.html"),
+                        WebSettings = { DefaultEncoding = "utf-8" },
+                        HeaderSettings = { FontSize = 8, Right = "Page [page] of [toPage]", Line = true, Spacing = 2.812 }
+                    }
+                }
+            };
+
+            var converter = new SynchronizedConverter(new PdfTools());
+            var bytes = converter.Convert(doc);
+            File.WriteAllBytes("__output__.pdf", bytes);
+        }
+    }
+}
